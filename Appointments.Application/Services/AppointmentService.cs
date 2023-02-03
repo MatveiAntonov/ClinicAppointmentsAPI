2ede76d91e8eaf0c21a114930206bbffa424418a
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 17:20:15 +0300

    Added services in Appointsments.Application project

---
+++ Appointments.Application/Services/AppointmentService.cs
@@ -0,0 +1,50 @@
+﻿
+
+using Appointments.Domain.Entities;
+using Profiles.Domain.Interfaces.Repositories;
+
+namespace Profiles.Application.Services
+{
+    public class AppointmentService : IAppointmentRepository
+    {
+        private readonly IAppointmentRepository _appointmentRepository;
+        public AppointmentService(IAppointmentRepository repository)
+        {
+            _appointmentRepository = repository;
+        }
+
+        public async Task<IEnumerable<Appointment?>> GetAllAppointments()
+        {
+            var appointments = await _appointmentRepository.GetAllAppointments();
+
+            return appointments;
+        }
+
+        public async Task<Appointment?> GetAppointment(int id)
+        {
+            var appointment = await _appointmentRepository.GetAppointment(id);
+
+            return appointment;
+        }
+
+        public async Task<Appointment?> CreateAppointment(Appointment appointment)
+        {
+            var createdAppointment = await _appointmentRepository.CreateAppointment(appointment);
+
+            return createdAppointment;
+        }
+
+        public async Task<Appointment?> UpdateAppointment(Appointment appointment)
+        {
+            var updatedAppointment = await _appointmentRepository.UpdateAppointment(appointment);
+
+            return updatedAppointment;
+        }
+        public async Task<Appointment?> DeleteAppointment(int id)
+        {
+            var deletedAppointment = await _appointmentRepository.DeleteAppointment(id);
+
+            return deletedAppointment;
+        }
+    }
+}
---
+++ Appointments.Application/Services/ResultService.cs
@@ -0,0 +1,49 @@
+﻿
+using Appointments.Domain.Entities;
+using Profiles.Domain.Interfaces.Services;
+
+namespace Profiles.Application.Services
+{
+    public class ResultService : IResultService
+    {
+        private readonly IResultService _resultRepository;
+        public ResultService(IResultService repository)
+        {
+            _resultRepository = repository;
+        }
+
+        public async Task<IEnumerable<Result?>> GetAllResults()
+        {
+            var results = await _resultRepository.GetAllResults();
+
+            return results;
+        }
+
+        public async Task<Result?> GetResult(int id)
+        {
+            var result = await _resultRepository.GetResult(id);
+
+            return result;
+        }
+
+        public async Task<Result?> CreateResult(Result result)
+        {
+            var createdResult = await _resultRepository.CreateResult(result);
+
+            return createdResult;
+        }
+
+        public async Task<Result?> UpdateResult(Result result)
+        {
+            var updatedResult = await _resultRepository.UpdateResult(result);
+
+            return updatedResult;
+        }
+        public async Task<Result?> DeleteResult(int id)
+        {
+            var deletedResult = await _resultRepository.DeleteResult(id);
+
+            return deletedResult;
+        }
+    }
+}
