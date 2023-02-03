dd2c3056c7fbf801c70783b6f387874c4433cf94
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 17:21:07 +0300

    Added entities to Appointments.Domain project

---
+++ Appointments.Domain/Entities/Appointment.cs
@@ -0,0 +1,19 @@
+﻿using Profiles.Domain.Entities.ForeignEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointments.Domain.Entities
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public string PatientId { get; set; } = String.Empty;
+        public string DoctorId { get; set; } = String.Empty;
+        public string ServiceId { get; set; } = String.Empty;
+        public DateTime? DateTime { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
---
+++ Appointments.Domain/Entities/Result.cs
@@ -0,0 +1,19 @@
+﻿using Profiles.Domain.Entities.ForeignEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointments.Domain.Entities
+{
+    public class Result
+    {
+        public int Id { get; set; }
+        public string Complaints { get; set; } = String.Empty;
+        public string Conclusion { get; set; } = String.Empty;
+        public string Recomendations { get; set; } = String.Empty;
+        public int AppointmentId { get; set; }
+        public Appointment Appointment { get; set; } = new();
+    }
+}
