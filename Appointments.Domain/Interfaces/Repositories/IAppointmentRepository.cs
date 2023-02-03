0e51bb828ebcae98c7872589602ec7ea5355d16d
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 17:21:27 +0300

    Added repository interfaces to Appointments.Domain

---
+++ Appointments.Domain/Interfaces/Repositories/IAppointmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using Appointments.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profiles.Domain.Interfaces.Repositories
+{
+    public interface IAppointmentRepository
+    {
+        Task<IEnumerable<Appointment?>> GetAllAppointments();
+        Task<Appointment?> GetAppointment(int id);
+        Task<Appointment?> CreateAppointment(Appointment appointment);
+        Task<Appointment?> UpdateAppointment(Appointment appointment);
+        Task<Appointment?> DeleteAppointment(int id);
+    }
+}
---
+++ Appointments.Domain/Interfaces/Repositories/IResultRepository.cs
@@ -0,0 +1,19 @@
+﻿using Appointments.Domain.Entities;
+using Profiles.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profiles.Domain.Interfaces.Repositories
+{
+    public interface IResultRepository
+    {
+        Task<IEnumerable<Result?>> GetAllResults();
+        Task<Result?> GetResult(int id);
+        Task<Result?> CreateResult(Result result);
+        Task<Result?> UpdateResult(Result result);
+        Task<Result?> DeleteResult(int id);
+    }
+}
