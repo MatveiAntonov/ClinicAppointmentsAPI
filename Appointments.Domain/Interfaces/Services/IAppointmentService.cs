173e24b8c156a7b0b158497c6bbeb2f6936e3eb5
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 17:21:51 +0300

    Added service interfaces to Appointments.Domain

---
+++ Appointments.Domain/Interfaces/Services/IAppointmentService.cs
@@ -0,0 +1,13 @@
+﻿using Appointments.Domain.Entities;
+
+namespace Profiles.Domain.Interfaces.Services
+{
+    public interface IAppointmentService
+    {
+        Task<IEnumerable<Appointment?>> GetAllAppointments();
+        Task<Appointment?> GetAppointment(int id);
+        Task<Appointment?> CreateAppointment(Appointment appointment);
+        Task<Appointment?> UpdateAppointment(Appointment appointment);
+        Task<Appointment?> DeleteAppointment(int id);
+    }
+}
---
+++ Appointments.Domain/Interfaces/Services/IResultService.cs
@@ -0,0 +1,13 @@
+﻿using Appointments.Domain.Entities;
+
+namespace Profiles.Domain.Interfaces.Services
+{
+    public interface IResultService
+    {
+        Task<IEnumerable<Result?>> GetAllResults();
+        Task<Result?> GetResult(int id);
+        Task<Result?> CreateResult(Result result);
+        Task<Result?> UpdateResult(Result result);
+        Task<Result?> DeleteResult(int id);
+    }
+}
