57dfe53a5833b992bc5f81d16b4a1fda23230aba
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 17:22:48 +0300

    Added database context  to Appointments.Persistence

---
+++ Appointments.Persistence/Contexts/AppointmentsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Appointments.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Profiles.Persistence.Contexts
+{
+    public class AppointmentsDbContext : DbContext, IAppointmentsDbContext
+    {
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Result> Results { get; set; }
+
+        public AppointmentsDbContext(DbContextOptions<AppointmentsDbContext> options)
+        : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new DocumentsConfiguration());
+            builder.ApplyConfiguration(new PhotoConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
