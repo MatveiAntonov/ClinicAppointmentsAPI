05dec9c37c7c3672fd5f1de374492cedaa988cdf
ProsTuha <tuhalf2002@mail.ru>
2023-02-07 13:31:51 +0300

    Added entities models

---
+++ Appointments.WebApi/Models/DTOs/AppointmentDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Appointments.WebApi.Models.DTOs
+{
+    public class AppointmentDto
+    {
+        public int Id { get; set; }
+        public string PatientId { get; set; } = String.Empty;
+        public string DoctorId { get; set; } = String.Empty;
+        public string? ServiceId { get; set; } = String.Empty;
+        public DateTime? DateTime { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
