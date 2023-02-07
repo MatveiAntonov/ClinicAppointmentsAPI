1af02a14a3f9dff7aa90fad7397b3d4de95c7606
ProsTuha <tuhalf2002@mail.ru>
2023-02-07 13:30:41 +0300

    Added DI for middleware

---
+++ Appointments.Persistence/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Appointments.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Appointments.Application.Services;
+using Appointments.Domain.Interfaces.Repositories;
+using Appointments.Domain.Interfaces.Services;
+using Appointments.Persistence.Contexts;
+
+namespace Appointments.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection ConnectDatabase(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<AppointmentsDbContext>(options => options
+            .UseNpgsql(connectionString));
+
+            services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+            services.AddScoped<IResultRepository, ResultRepository>();
+
+            services.AddScoped<IAppointmentService, AppointmentService>();
+            services.AddScoped<IResultService, ResultService>();
+
+            return services;
+        }
+    }
+}
