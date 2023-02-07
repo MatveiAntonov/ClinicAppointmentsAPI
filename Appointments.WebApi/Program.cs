04b74b862ff8fbdd71dbcd3a79bf059e7d13b77f
ProsTuha <tuhalf2002@mail.ru>
2023-02-07 13:32:20 +0300

    Added programm class

--- Appointments.WebApi/Program.cs
+++ Appointments.WebApi/Program.cs
@@ -1,6 +1,24 @@
+using Appointments.Persistence;
+using Appointments.WebApi.Extensions;
+using Appointments.WebApi.Mappings;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+
+services.ConnectDatabase(builder.Configuration.GetConnectionString("AppointmentsConnection"));
+
+services.AddAutoMapper(typeof(MappingProfile));
+
+services.AddControllers();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.ConfigureExceptionHandler();
 
 app.Run();
