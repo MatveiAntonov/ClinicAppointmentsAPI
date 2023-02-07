fbaa11ba7e9a68ddee41aba56f3545a55b3f18d1
ProsTuha <tuhalf2002@mail.ru>
2023-02-07 13:30:01 +0300

    Added configurations for entities

---
+++ Appointments.Persistence/EntityTypeConfigurations/AppointmentsConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Appointments.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Appointments.Persistence.EntityTypeConfigurations
+{
+    public class AppointmentsConfiguration : IEntityTypeConfiguration<Appointment>
+    {
+        public void Configure(EntityTypeBuilder<Appointment> builder)
+        {
+            builder.HasKey(appointment => appointment.Id);
+            builder.HasIndex(appointment => appointment.Id).IsUnique();
+            builder.Property(appointment => appointment.DoctorId)
+                .IsRequired();
+            builder.Property(appointment => appointment.PatientId)
+                .IsRequired();
+            builder.Property(appointment => appointment.DateTime)
+                .HasColumnType("timestamp");
+        }
+    }
+}
---
+++ Appointments.Persistence/EntityTypeConfigurations/ResultsConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Appointments.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Appointments.Persistence.EntityTypeConfigurations
+{
+    public class ResultsConfiguration : IEntityTypeConfiguration<Result>
+    {
+        public void Configure(EntityTypeBuilder<Result> builder)
+        {
+            builder.HasKey(result => result.Id);
+            builder.HasIndex(result => result.Id).IsUnique();
+            builder.HasOne(result => result.Appointment)
+                .WithOne()
+                .HasForeignKey<Result>(result => result.AppointmentId);
+        }
+    }
+}
