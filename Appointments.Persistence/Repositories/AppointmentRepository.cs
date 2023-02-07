007cfa6c59ab0fb51ba58f654da00c9df8f0ff94
ProsTuha <tuhalf2002@mail.ru>
2023-02-07 13:30:26 +0300

    Added repositories

---
+++ Appointments.Persistence/Repositories/AppointmentRepository.cs
@@ -0,0 +1,77 @@
+﻿using Appointments.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Appointments.Domain.Interfaces.Repositories;
+using Appointments.Persistence.Contexts;
+
+namespace Appointments.Persistence.Repositories
+{
+    public class AppointmentRepository : IAppointmentRepository
+    {
+        private readonly AppointmentsDbContext _appointmentsDbContext;
+
+        public AppointmentRepository(AppointmentsDbContext appointmentsDbContext)
+        {
+            _appointmentsDbContext = appointmentsDbContext;
+        }
+        public async Task<IEnumerable<Appointment?>> GetAllAppointments()
+        {
+            return await _appointmentsDbContext.Appointments.ToListAsync();
+        }
+        public async Task<Appointment?> GetAppointment(int id)
+        {
+            return await _appointmentsDbContext.Appointments
+                .FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task<bool> CreateAppointment(Appointment appointment)
+        {
+            try
+            {
+                await _appointmentsDbContext.Appointments.AddAsync(appointment);
+                _appointmentsDbContext.SaveChanges();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateAppointment(Appointment appointment, Appointment entity)
+        {
+            try
+            {
+                appointment.PatientId = entity.PatientId;
+                appointment.DoctorId = entity.DoctorId;
+                appointment.ServiceId = entity.ServiceId;
+                appointment.DateTime = entity.DateTime;
+                appointment.IsApproved = entity.IsApproved;
+
+                _appointmentsDbContext.SaveChanges();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteAppointment(Appointment appointment)
+        {
+            try
+            {
+                _appointmentsDbContext.Appointments.Remove(appointment);
+                _appointmentsDbContext.SaveChanges();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Appointments.Persistence/Repositories/ResultRepository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Appointments.Persistence.Contexts;
+using Appointments.Domain.Interfaces.Repositories;
+using Appointments.Domain.Entities;
+
+namespace Appointments.Persistence.Repositories
+{
+    public class ResultRepository : IResultRepository
+    {
+        private readonly AppointmentsDbContext _appointmentsDbContext;
+
+        public ResultRepository(AppointmentsDbContext appointmentsDbContext)
+        {
+            _appointmentsDbContext = appointmentsDbContext;
+        }
+        public async Task<IEnumerable<Result?>> GetAllResults()
+        {
+            return await _appointmentsDbContext.Results.ToListAsync();
+        }
+        public async Task<Result?> GetResult(int id)
+        {
+            return await _appointmentsDbContext.Results
+                .FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task<bool> CreateResult(Result result)
+        {
+            try
+            {
+                await _appointmentsDbContext.Results.AddAsync(result);
+                _appointmentsDbContext.SaveChanges();
+
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateResult(Result result, Result entity)
+        {
+            try
+            {
+                result.Complaints = entity.Complaints;
+                result.Conclusion = entity.Conclusion;
+                result.Recomendations = entity.Recomendations;
+                result.AppointmentId = entity.AppointmentId;
+
+                _appointmentsDbContext.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteResult(Result result)
+        {
+            try
+            {
+                _appointmentsDbContext.Results.Remove(result);
+                _appointmentsDbContext.SaveChanges();
+
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
