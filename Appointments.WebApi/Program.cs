bc8c35954f1b23f6eea13dadfbec3a3fdee21c15
ProsTuha <tuhalf2002@mail.ru>
2023-02-21 09:41:13 +0300

    Added MassTransit to middleware

--- Appointments.WebApi/Program.cs
+++ Appointments.WebApi/Program.cs
@@ -1,6 +1,8 @@
+using Appointments.Application.Consumer.Events;
 using Appointments.Persistence;
 using Appointments.WebApi.Extensions;
 using Appointments.WebApi.Mappings;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
@@ -9,6 +11,18 @@ var services = builder.Services;
 services.ConnectDatabase(builder.Configuration.GetConnectionString("AppointmentsConnection"));
 
 services.AddAutoMapper(typeof(MappingProfile));
+
+services.AddMassTransit(x =>
+{
+    x.AddConsumer<ServiceCreatedConsumer>();
+    x.AddConsumer<ServiceDeletedConsumer>();
+    x.AddConsumer<ServiceUpdatedConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 services.AddControllers();
 var app = builder.Build();
