bae198979b6314573067b54e454290ff13d50bfc
ProsTuha <tuhalf2002@mail.ru>
2023-02-21 09:33:57 +0300

    Added messages from RabbitMq for Service entity

---
+++ Appointments.Application/Consumer/Messages/ServiceCreated.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+
+namespace Events
+{
+    public class ServiceCreated
+    {
+        public int Id { get; set; }
+        public string ServiceCategoryName { get; set; } = String.Empty;
+        public string ServiceName { get; set; } = String.Empty;
+        public double? ServicePrice { get; set; }
+        public string SpecializationName { get; set; } = String.Empty;
+        public DateTime? TimeSlotSize { get; set; }
+    }
+}
