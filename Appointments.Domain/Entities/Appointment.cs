ccae4617d3a92467b8c4517fa269fb4d8f657dcf
ProsTuha <tuhalf2002@mail.ru>
2023-02-21 09:26:18 +0300

    Fixed datatypes for properties

--- Appointments.Domain/Entities/Appointment.cs
+++ Appointments.Domain/Entities/Appointment.cs
@@ -4,9 +4,9 @@ namespace Appointments.Domain.Entities
     public class Appointment
     {
         public int Id { get; set; }
-        public string PatientId { get; set; } = String.Empty;
-        public string DoctorId { get; set; } = String.Empty;
-        public string? ServiceId { get; set; } = String.Empty;
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public int? ServiceId { get; set; }
         public DateTime? DateTime { get; set; }
         public bool IsApproved { get; set; }
     }
