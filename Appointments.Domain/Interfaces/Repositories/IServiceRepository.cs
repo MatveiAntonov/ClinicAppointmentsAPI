7919e09245cdcfc4278f03eed05f82366dff3d7b
ProsTuha <tuhalf2002@mail.ru>
2023-02-21 09:26:43 +0300

    Added interface for ServiceRepository

---
+++ Appointments.Domain/Interfaces/Repositories/IServiceRepository.cs
@@ -0,0 +1,11 @@
+﻿using Appointments.Domain.Entities;
+
+namespace Appointments.Domain.Interfaces.Repositories
+{
+    public interface IServiceRepository
+    {
+        Task<bool> CreateService(Service service);
+        bool UpdateService(Service service);
+        bool DeleteService(Service service);
+    }
+}
