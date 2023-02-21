dcc58885c445555e99c655760043e1d1d6d23ad7
ProsTuha <tuhalf2002@mail.ru>
2023-02-21 09:35:34 +0300

    Added Consumer event for RabbitMq

---
+++ Appointments.Application/Consumer/Events/ServiceCreatedConsumer.cs
@@ -0,0 +1,29 @@
+﻿using Events;
+using Appointments.Domain.Entities;
+using Appointments.Domain.Interfaces.Repositories;
+using AutoMapper;
+using MassTransit;
+
+namespace Appointments.Application.Consumer.Events
+{
+    public class ServiceCreatedConsumer : IConsumer<ServiceCreated>
+    {
+        private readonly IServiceRepository _serviceRepository;
+        private readonly IMapper _mapper;
+
+        public ServiceCreatedConsumer(IServiceRepository serviceRepository, IMapper mapper)
+        {
+            _serviceRepository = serviceRepository;
+            _mapper = mapper;
+        }
+
+        public async Task Consume(ConsumeContext<ServiceCreated> context)
+        {
+            var service = _mapper.Map<Service>(context.Message);
+            if (service is not null)
+            {
+                await _serviceRepository.CreateService(service);
+            }
+        }
+    }
+}
---
+++ Appointments.Application/Consumer/Events/ServiceUpdatedConsumer.cs
@@ -0,0 +1,27 @@
+﻿using Events;
+using Appointments.Domain.Entities;
+using Appointments.Domain.Interfaces.Repositories;
+using AutoMapper;
+using MassTransit;
+
+namespace Appointments.Application.Consumer.Events
+{
+    public class ServiceUpdatedConsumer : IConsumer<ServiceUpdated>
+    {
+        private readonly IServiceRepository _serviceRepository;
+        private readonly IMapper _mapper;
+        public ServiceUpdatedConsumer(IServiceRepository serviceRepository, IMapper mapper)
+        {
+            _serviceRepository = serviceRepository;
+            _mapper = mapper;
+        }
+        public async Task Consume(ConsumeContext<ServiceUpdated> context)
+        {
+            var service = _mapper.Map<Service>(context.Message);
+            if (service is not null)
+            {
+                _serviceRepository.UpdateService(service);
+            }
+        }
+    }
+}
