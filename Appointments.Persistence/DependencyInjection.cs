62d2854702db2ed2973a7b5ab60989e5a04b80f8
ProsTuha <tuhalf2002@mail.ru>
2023-02-22 18:05:45 +0300

    Enabled UTC supporting

--- Appointments.Persistence/DependencyInjection.cs
+++ Appointments.Persistence/DependencyInjection.cs
@@ -12,12 +12,16 @@ namespace Appointments.Persistence
     {
         public static IServiceCollection ConnectDatabase(this IServiceCollection services, string connectionString)
         {
-            services.AddDbContext<AppointmentsDbContext>(options => options
-            .UseNpgsql(connectionString));
+            services.AddDbContext<AppointmentsDbContext>(options => {
+                options.UseNpgsql(connectionString);
+                AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            });
 
             services.AddScoped<IAppointmentRepository, AppointmentRepository>();
             services.AddScoped<IResultRepository, ResultRepository>();
             services.AddScoped<IServiceRepository, ServiceRepository>();
+            services.AddScoped<IDoctorRepository, DoctorRepository>();
+            services.AddScoped<IPatientRepository, PatientRepository>();
 
             services.AddScoped<IAppointmentService, AppointmentService>();
             services.AddScoped<IResultService, ResultService>();
