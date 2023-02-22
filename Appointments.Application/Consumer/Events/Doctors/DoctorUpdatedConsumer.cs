66b254d6b85da7f41e931b0afb59ad2c476b8dcc
ProsTuha <tuhalf2002@mail.ru>
2023-02-22 17:39:13 +0300

    Added events for Doctor entity for message broker

---
+++ Appointments.Application/Consumer/Events/Doctors/DoctorUpdatedConsumer.cs
@@ -0,0 +1,27 @@
+﻿using Appointments.Domain.Entities;
+using Appointments.Domain.Interfaces.Repositories;
+using AutoMapper;
+using MassTransit;
+using Events;
+
+namespace Appointments.Application.Consumer.Events.Doctors
+{
+    public class DoctorUpdatedConsumer : IConsumer<DoctorUpdated>
+    {
+        private readonly IDoctorRepository _doctorRepository;
+        private readonly IMapper _mapper;
+        public DoctorUpdatedConsumer(IDoctorRepository doctorRepository, IMapper mapper)
+        {
+            _doctorRepository = doctorRepository;
+            _mapper = mapper;
+        }
+        public async Task Consume(ConsumeContext<DoctorUpdated> context)
+        {
+            var doctor = _mapper.Map<Doctor>(context.Message);
+            if (doctor is not null)
+            {
+                _doctorRepository.UpdateDoctor(doctor);
+            }
+        }
+    }
+}
