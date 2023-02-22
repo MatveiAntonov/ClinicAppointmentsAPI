8bba8efe0065acf1ba9847205959000429bde143
ProsTuha <tuhalf2002@mail.ru>
2023-02-22 17:34:47 +0300

    Added foreign entities: Doctor & Patient

---
+++ Appointments.Domain/Entities/Doctor.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Appointments.Domain.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string? MiddleName { get; set; } = String.Empty;
+        public string SpecializationName { get; set; } = String.Empty;
+        public string PhotoUrl { get; set; } = String.Empty;
+        public string? OfficeName { get; set; } = String.Empty;
+        public string OfficeCity { get; set; } = String.Empty;
+        public string OfficeRegion { get; set; } = String.Empty;
+        public string OfficeStreet { get; set; } = String.Empty;
+        public string? OfficePostalCode { get; set; } = String.Empty;
+        public int OfficeHouseNumber { get; set; }
+    }
+}
---
+++ Appointments.Domain/Entities/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace Appointments.Domain.Entities
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string? MiddleName { get; set; } = String.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public string PhotoUrl { get; set; } = String.Empty;
+    }
+}
