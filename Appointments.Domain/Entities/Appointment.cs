c454369cf440a171d077d5ada48c727bec3707d3
ProsTuha <tuhalf2002@mail.ru>
2023-02-22 17:35:08 +0300

    Added navigation properties for Appointment entity

--- Appointments.Domain/Entities/Appointment.cs
+++ Appointments.Domain/Entities/Appointment.cs
@@ -5,8 +5,11 @@ namespace Appointments.Domain.Entities
     {
         public int Id { get; set; }
         public int PatientId { get; set; }
+        public Patient Patient { get; set; } = new();
         public int DoctorId { get; set; }
+        public Doctor? Doctor { get; set; } = new();
         public int? ServiceId { get; set; }
+        public Service? Service { get; set; } = new();
         public DateTime? DateTime { get; set; }
         public bool IsApproved { get; set; }
     }
