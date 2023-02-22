5d3733446072e8691cf87c661d908c8b6588dd3e
ProsTuha <tuhalf2002@mail.ru>
2023-02-22 17:37:08 +0300

    Added messages models for Patient entity for message broker

---
+++ Appointments.Application/Consumer/Messages/Patients/PatientDeleted.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class PatientDeleted
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string? MiddleName { get; set; } = String.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public string PhotoUrl { get; set; } = string.Empty;
+    }
+}
