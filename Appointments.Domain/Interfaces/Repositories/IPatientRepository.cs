9db94d6ac1e08e6383eb43d9940084cf6b18a835
ProsTuha <tuhalf2002@mail.ru>
2023-02-22 17:35:44 +0300

    Added repository interfaces for Doctor and Patient entities

---
+++ Appointments.Domain/Interfaces/Repositories/IPatientRepository.cs
@@ -0,0 +1,11 @@
+﻿using Appointments.Domain.Entities;
+
+namespace Appointments.Domain.Interfaces.Repositories
+{
+    public interface IPatientRepository
+    {
+        Task<bool> CreatePatient(Patient patient);
+        bool UpdatePatient(Patient patient);
+        bool DeletePatient(Patient patient);
+    }
+}
