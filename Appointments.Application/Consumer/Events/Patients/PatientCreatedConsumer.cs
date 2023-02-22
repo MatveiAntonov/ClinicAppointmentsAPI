a78b4928bd117d2d21920c4fd2ec7c17845c2871
ProsTuha <tuhalf2002@mail.ru>
2023-02-22 17:39:48 +0300

    Added events for Patient entity for message broker

---
+++ Appointments.Application/Consumer/Events/Patients/PatientCreatedConsumer.cs
@@ -0,0 +1,34 @@
+﻿using Appointments.Domain.Entities;
+using Appointments.Domain.Interfaces.Repositories;
+using AutoMapper;
+using Events;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointments.Application.Consumer.Events.Patients
+{
+    public class PatientCreatedConsumer : IConsumer<PatientCreated>
+    {
+        private readonly IPatientRepository _patientRepository;
+        private readonly IMapper _mapper;
+
+        public PatientCreatedConsumer(IPatientRepository patientRepository, IMapper mapper)
+        {
+            _patientRepository = patientRepository;
+            _mapper = mapper;
+        }
+
+        public async Task Consume(ConsumeContext<PatientCreated> context)
+        {
+            var patient = _mapper.Map<Patient>(context.Message);
+            if (patient is not null)
+            {
+                await _patientRepository.CreatePatient(patient);
+            }
+        }
+    }
+}
